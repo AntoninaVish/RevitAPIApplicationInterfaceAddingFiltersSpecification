282595e1fbaf99220621b94040edebc5459cd638
AntoninaVish <vishivtonya@gmail.com>
2023-03-12 14:32:24 +0300

    Добавьте файлы проекта.

---
+++ MainViewViewModel.cs
@@ -0,0 +1,108 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using RevitAPIApplicationFiltersSpecificationLibrary;
+using RevitAPIApplicationInterfaceAddingFiltersSpecification.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIApplicationInterfaceAddingFiltersSpecification
+{
+    public class MainViewViewModel
+    {
+
+        private ExternalCommandData _commandData;
+
+        private Document _doc;
+
+        public List<ScheduleWrapper> Schedules { get; }
+
+        public DelegateCommand AddFilterCommand { get; }
+
+        public string ParameterName { get; set; }
+
+        public string ParameterValue { get; set; }
+
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            _doc = _commandData.Application.ActiveUIDocument.Document;
+            Schedules = ScheduleUtils.GetAllTheSchedules(_doc).Select(s => new ScheduleWrapper(s)).ToList();
+            AddFilterCommand = new DelegateCommand(OnAddFilterCommand);
+        }
+
+        private void OnAddFilterCommand()
+        {
+            List<ScheduleWrapper> selectedSchedules = Schedules.Where(s => s.IsSelected).ToList();
+            if (selectedSchedules == null)
+                return;
+
+            using (var ts = new Transaction(_doc, "Add filter to schedule"))
+            {
+                ts.Start();
+                foreach (var schedule in selectedSchedules)
+                {
+                    var scheduleDef = schedule.ViewSchedule.Definition;
+                    if (scheduleDef == null)
+                        continue;
+
+                    ScheduleField field = FindField(schedule.ViewSchedule, ParameterName);
+                    if (field == null)
+                    {
+                        SchedulableField schedulableField = FindSchedulableField(schedule.ViewSchedule, ParameterName);
+                        if (schedulableField == null)
+                            continue;
+
+                        field = scheduleDef.AddField(schedulableField);
+                    }
+
+                    if (field == null)
+                        continue;
+
+                    var filter = new ScheduleFilter(field.FieldId, ScheduleFilterType.Equal, ParameterValue);
+                    if (filter == null)
+                        continue;
+
+                    scheduleDef.AddFilter(filter);
+                }
+                ts.Commit();
+            }
+           
+        }
+
+        private SchedulableField FindSchedulableField(ViewSchedule viewSchedule, string parameterName)
+        {
+            var schedulableFied = viewSchedule.Definition.GetSchedulableFields()
+                                    .FirstOrDefault(p => p.GetName(viewSchedule.Document) == parameterName);
+            return schedulableFied;
+        }
+
+        private ScheduleField FindField(ViewSchedule viewSchedule, string parameterName)
+        {
+            ScheduleDefinition definition = viewSchedule.Definition;
+            var fieldCount = definition.GetFieldCount();
+
+            for (int i = 0; i < fieldCount; i++)
+            {
+                var field = definition.GetField(i);
+                var fieldName = field.GetName();
+
+                if (fieldName == parameterName)
+                    return definition.GetField(i);
+            }
+            return null;
+        }
+
+        public event EventHandler CloseRequest;
+
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+          
+    }
+}
---
+++ Wrappers/ScheduleWrapper.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIApplicationInterfaceAddingFiltersSpecification.Wrappers
+{
+    public class ScheduleWrapper
+    {
+
+        public ScheduleWrapper(ViewSchedule viewSchedule)
+        {
+            ViewSchedule = viewSchedule;
+            Name = viewSchedule.Name;
+
+        }
+
+        public bool IsSelected { get; set; }
+
+        public ViewSchedule ViewSchedule { get; }
+        
+        public string Name { get; }
+    }
+}
